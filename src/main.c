508f70828fa85e8348100284de2464f87957bee3
shrub <shrub719@gmail.com>
2026-05-13 15:00:12 +0100

    nuke (untested)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int arg, char **argv) {
-    printf("remember: ghidra addresses are 0x00100000 too big!\n");
+#define OUTPUT "code.ips"
+
+void writeInt(long n, size_t len, FILE *fptr) {
+    while (len > 0) {
+        unsigned char byte = (n >> (len - 1)) % 0x100;
+        fwrite(&byte, 1, 1, fptr);
+        len--;
+    }
+}
+
+void nuke(long offset) {
+    FILE *fptr = fopen(OUTPUT, "wb");
+
+    fwrite("PATCH", 1, 5, fptr);
+
+    writeInt(offset, 3, fptr);
+    writeInt(0x1, 2, fptr);
+
+    // bkpt instruction
+    writeInt(0x700020e1, 4, fptr);
+
+    fwrite("EOF", 1, 3, fptr);
+
+    fclose(fptr);
+}
+
+int main(int argc, char **argv) {
+    printf("(remember: ghidra addresses are 0x00100000 too big!)\n");
+
+    long offset = strtol(argv[1], NULL, 16);
+
+    nuke(offset);
+    printf("made a nuke for code.bin offset 0x%lx\n", offset);
 
     return 0;
 }
